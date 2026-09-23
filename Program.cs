2bfd3bbbf9e314389ce1805f7af81f286ff2a576
Knightmare3550 <jaime.rodriguez2550@outlook.com>
2026-09-23 13:04:06 -0700

    Mini Challenge #4

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter first number");
+Double firstNumber = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("write second number");
+Double secondNumber = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("The sum of the two numbers is: " + (firstNumber + secondNumber));
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("The first number is greater than the second number.");
+}
+else if (firstNumber < secondNumber)
+{
+    Console.WriteLine("The first number is less than the second number.");
+}
+else
+{
+    Console.WriteLine("The first number is equal to the second number.");
+}
+
+
